488093202f1bfec66e25b48c06b693755494e07d
dhanavelcse <dhanavelcse@gmail.com>
2024-04-04 13:21:35 -0400

    Adding temporary changes to Github

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS=['localhost','https://dhanalaksh26-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS=['https://dhanalaksh26-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
